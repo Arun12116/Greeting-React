d6ef5a538dfcef5da265bc8c02087650541a4627
arun <yarunkumar136@gmail.com>
2021-07-01 15:47:27 +0530

    greeting

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+let curDate=new Date();
+curDate=curDate.getHours();
+let greeting="";
+const cssstyle={};
+if(curDate>=1 && curDate<12){
+  greeting="Good morning";
+  cssstyle.color="green";
+}else if(curDate>=12 && curDate<19){
+  greeting="Good Afternoon";
+  cssstyle.color="orange";
 
+}else{
+  greeting="Good night";
+  cssstyle.color="green";
+
+}
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  <div className="heading">
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ 
+  <h1>Hello Sir,<span style={cssstyle}>{greeting}</span></h1>
+  </div>,
+ 
+  document.getElementById("root")
+
+  
+);
---
+++ tccc.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+    <>
+    <p>arun</p>
+    </>,
+    document.getElementById("root")
+);
